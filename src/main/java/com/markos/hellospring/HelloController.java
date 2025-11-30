98bb750042b0d90056398e57bb0bf48366f230fc
Markos <fdnerh@>
2025-11-30 00:19:10 -0600

    Hello Spring Boot app

---
+++ src/main/java/com/markos/hellospring/HelloController.java
@@ -0,0 +1,21 @@
+package com.markos.hellospring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String greeting(Model model) {
+        String name = "Markos";
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+}
